243d0e39f0a1dc7d9e4eb28c754f5e728c1a0324
bad018 <alicedb173@gmail.com>
2024-05-12 18:20:59 +0300

    Update test_polyedr.py

--- tests/test_polyedr.py
+++ tests/test_polyedr.py
@@ -23,20 +23,45 @@ class TestPolyedr(unittest.TestCase):
 4	3    7    8    4
 4	1    4    8    5"""
         fake_file_path = 'data/holey_box.geom'
+        new_fake_file_content = """200.0	45.0	45.0	30.0
+8	6	24
+-1.0	-1.0	1.1
+-1.0	1.0	1.1
+1.0	1.0	1.1
+1.0	-1.0	1.1
+-1.0	-1.0	-1.1
+-1.0	1.0	-1.1
+1.0	1.0	-1.1
+1.0	-1.0	-1.1
+4	1    2    3    4
+4	5    6    2    1
+4	3    2    6    7
+4	3    7    8    4
+4	1    4    8    5
+4	8    7    6    5"""
+        new_fake_file_path = 'data/holey_cube.geom'
         with patch('shadow.polyedr.open'.format(__name__),
                    new=mock_open(read_data=fake_file_content)) as _file:
-            self.polyedr = Polyedr(fake_file_path)
+            self.polyedr1 = Polyedr(fake_file_path)
             _file.assert_called_once_with(fake_file_path)
 
+        with patch('shadow.polyedr.open'.format(__name__),
+                   new=mock_open(read_data=new_fake_file_content)) as _file:
+            self.polyedr2 = Polyedr(new_fake_file_path)
+            _file.assert_called_once_with(new_fake_file_path)
+
     def test_num_vertexes(self):
-        self.assertEqual(len(self.polyedr.vertexes), 8)
+        self.assertEqual(len(self.polyedr1.vertexes), 8)
 
     def test_num_facets(self):
-        self.assertEqual(len(self.polyedr.facets), 4)
+        self.assertEqual(len(self.polyedr1.facets), 4)
 
     def test_num_edges(self):
-        self.assertEqual(len(self.polyedr.edges), 16)
+        self.assertEqual(len(self.polyedr1.edges), 16)
 
     # Сумма площадей «хороших» граней
-    def test_sum_area(self):
-        self.assertEqual(self.polyedr.sum_area(), 0.0)
+    def test_sum_area01(self):
+        self.assertEqual(self.polyedr1.sum_area(), 0.0)
+
+    def test_sum_area02(self):
+        self.assertAlmostEqual(self.polyedr2.sum_area(), 8.0)
